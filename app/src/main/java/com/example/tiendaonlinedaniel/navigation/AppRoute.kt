eef2ccb2d695c514e3a174b68b52b891f1846ec5
Daniel <danielbeta97@gmail.com>
2025-10-01 21:54:35 -0300

    Creación de eventos

---
+++ app/src/main/java/com/example/tiendaonlinedaniel/navigation/AppRoute.kt
@@ -0,0 +1,14 @@
+package com.example.tiendaonlinedaniel.navigation
+
+sealed class AppRoute(val route: String) {
+    data object Home: AppRoute("home")
+    data object Profile: AppRoute("profile")
+    data object Settings: AppRoute("settings")
+
+    data class Detail(val itemId:String): AppRoute("detail/{itemId}")
+    {
+        fun buildRoute(): String{
+            return route.replace("{itemId}", newValue = itemId)
+        }
+    }
+}
